78b5897b729353db333598e34cdf1a5170a18fc2
wujie <wujie@mudu.tv>
2021-11-05 15:00:44 +0800

    04 lession

---
+++ 08-week/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [["@babel/plugin-transform-react-jsx", {pragma: "createElement"}]]
+          }
+        }
+      }
+    ]
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, ''),
+    },
+    compress: true,
+    port: 8080,
+  },
+}
